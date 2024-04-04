2902c5a996e5c959d1e2bdb4953aea8ac2c3c99e
yousef <yousef@gmail.com>
2024-04-04 11:16:45 +0330

    app.js file updated

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import  { Box, Typography } from '@mui/material'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+        <Box>
+            <Box>
+                <img src='assets/homepagePhoto.png'/>
+                <Typography>
+                    Welcome to the home page of our website
+                </Typography>
+            </Box>
+            <Box sx={{display: "flex", flexDirection:"column"}}>
+
+            </Box>
+        </Box>
+    </>
   )
 }
