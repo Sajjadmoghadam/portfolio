50889651147d8b5bd07a9b0f3b97a3dc9a343046
yousef <yousef@gmail.com>
2024-04-04 11:25:42 +0330

    Routes are created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import Work from './pages/Work'
+import Contact from './pages/Contact'
+import Services from './pages/Services'
 
 export default function App() {
   return (
     <>
-      <Home />
+      
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/services' element={<Services />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/work' element={<Work />}/>
+        <Route path='/contact' element={<Contact />}/>
+      </Routes>
     </>
   )
 }
