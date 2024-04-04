88421fed1601543a76cee2d24a33ff82e61f4560
AmirrezMirshafiee <amirrezamirshafiee0@gmail.com>
2024-04-04 13:15:48 +0330

    update services

--- src/pages/Services/index.jsx
+++ src/pages/Services/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Stack, Box, Typography, Button } from "@mui/material";
+import { Stack, Box, Typography, Button, Divider } from "@mui/material";
 import SouthIcon from "@mui/icons-material/South";
 export default function Services() {
   return (
@@ -9,6 +9,7 @@ export default function Services() {
           width: "100%",
           height: "100%",
           backgroundColor: "#000000	",
+          flexDirection: "column",
         }}
       >
         <Stack
@@ -51,9 +52,144 @@ export default function Services() {
             </Button>
           </Box>
 
-          <Box mt={5} sx={{width:'100%', height:'100%'}}>
-                <img width='50%' height='100%' src='assets/servicesimg1.png'/>
+          <Box mt={5} sx={{ width: "100%", height: "100%" }}>
+            <img width="80%" height="100%" src="assets/servicesimg1.png" />
+          </Box>
+        </Stack>
+        <Stack
+          sx={{
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <Stack
+            mt={10}
+            sx={{
+              backgroundColor: "#383735",
+              alignContent: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              mx: "auto",
+              textAlign: "center",
+              width: "70%",
+              padding: "40px",
+            }}
+          >
+            {" "}
+            <Box
+              sx={{
+                alignContent: "start",
+                justifyContent: "flex-start",
+                flexDirection: "column",
+                textAlign: "start",
+              }}
+            >
+              <Typography variant="h6" color="#DAC5A7" fontSize="13px">
+                Webdesign
+              </Typography>
+              <Typography variant="h4" color="#DAC5A7" fontSize="25px">
+                Transforming Your Ideas into Reality
+              </Typography>
             </Box>
+            <Box mt={2} sx={{ width: "100%", height: "100%" }}>
+              <img width="100%" height="100%" src="assets/servicesimg3.jfif" />
+            </Box>
+            <Box mt={5} width="100%" height="100%">
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Typography
+                  color="#DAC5A7"
+                  textAlign="start"
+                  width="250px"
+                  height="21px"
+                >
+                  Concept
+                </Typography>
+                <Typography
+                  color="#DAC5A799"
+                  textAlign="start"
+                  width="814px"
+                  height="58px"
+                >
+                  I take time to understand your business needs and audience to
+                  develop a unique concept for your website. I'll create
+                  wireframes that serve as the foundation for your site's design
+                  and functionality.
+                </Typography>
+              </Box>
+              <Divider
+                sx={{
+                  borderColor: "#DAC5A726",
+                  margin: "20px",
+                  height: "100%",
+                }}
+              />
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Typography
+                  color="#DAC5A7"
+                  textAlign="start"
+                  width="250px"
+                  height="21px"
+                >
+                  CMS Integration
+                </Typography>
+                <Typography
+                  color="#DAC5A799"
+                  textAlign="start"
+                  width="814px"
+                  height="58px"
+                >
+                  I can help you streamline your content management process by
+                  integrating a CMS into your website. Say goodbye to manual
+                  updates and hello to efficiency.
+                </Typography>
+              </Box>
+              <Divider
+                sx={{
+                  borderColor: "#DAC5A726",
+                  margin: "20px",
+                  height: "100%",
+                }}
+              />
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Typography
+                  color="#DAC5A7"
+                  textAlign="start"
+                  width="250px"
+                  height="21px"
+                >
+                  WEb Design System
+                </Typography>
+                <Typography
+                  color="#DAC5A799"
+                  textAlign="start"
+                  width="814px"
+                  height="58px"
+                >
+                  I use a modular design approach to create a web design system
+                  that ensures consistency throughout your website. This results
+                  in a professional and cohesive online presence.
+                </Typography>
+              </Box>
+            </Box>
+          </Stack>
         </Stack>
       </Stack>
     </>
