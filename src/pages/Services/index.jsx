05c9378bac48d83ec510be1e0fdc46f70df3cc19
AmirrezMirshafiee <amirrezamirshafiee0@gmail.com>
2024-04-04 13:31:23 +0330

    update services

--- src/pages/Services/index.jsx
+++ src/pages/Services/index.jsx
@@ -191,6 +191,273 @@ export default function Services() {
             </Box>
           </Stack>
         </Stack>
+        <Stack
+          sx={{
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <Stack
+            mt={5}
+            sx={{
+              backgroundColor: "#383735",
+              alignContent: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              mx: "auto",
+              textAlign: "center",
+              width: "70%",
+              padding: "40px",
+            }}
+          >
+            {" "}
+            <Box
+              sx={{
+                alignContent: "start",
+                justifyContent: "flex-start",
+                flexDirection: "column",
+                textAlign: "start",
+              }}
+            >
+              <Typography variant="h6" color="#DAC5A7" fontSize="13px">
+                Development
+              </Typography>
+              <Typography variant="h4" color="#DAC5A7" fontSize="25px">
+                Developing High-Performance Websites and Web Applications{" "}
+              </Typography>
+            </Box>
+            <Box mt={2} sx={{ width: "100%", height: "100%" }}>
+              <img width="100%" height="100%" src="assets/servicesimg2.jfif" />
+            </Box>
+            <Box mt={5} width="100%" height="100%">
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Typography
+                  color="#DAC5A7"
+                  textAlign="start"
+                  width="250px"
+                  height="21px"
+                >
+                  Framer
+                </Typography>
+                <Typography
+                  color="#DAC5A799"
+                  textAlign="start"
+                  width="814px"
+                  height="58px"
+                >
+                  I specialize in developing web applications using Framer. From
+                  custom animations to complex interactions, I bring your web
+                  app to life.
+                </Typography>
+              </Box>
+              <Divider
+                sx={{
+                  borderColor: "#DAC5A726",
+                  margin: "20px",
+                  height: "100%",
+                }}
+              />
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Typography
+                  color="#DAC5A7"
+                  textAlign="start"
+                  width="250px"
+                  height="21px"
+                >
+                  CMS Integration
+                </Typography>
+                <Typography
+                  color="#DAC5A799"
+                  textAlign="start"
+                  width="814px"
+                  height="58px"
+                >
+                  I can help you streamline your content management process by
+                  integrating a CMS into your website. Say goodbye to manual
+                  updates and hello to efficiency.
+                </Typography>
+              </Box>
+              <Divider
+                sx={{
+                  borderColor: "#DAC5A726",
+                  margin: "20px",
+                  height: "100%",
+                }}
+              />
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Typography
+                  color="#DAC5A7"
+                  textAlign="start"
+                  width="250px"
+                  height="21px"
+                >
+                  WEb Design System
+                </Typography>
+                <Typography
+                  color="#DAC5A799"
+                  textAlign="start"
+                  width="814px"
+                  height="58px"
+                >
+                  I use a modular design approach to create a web design system
+                  that ensures consistency throughout your website. This results
+                  in a professional and cohesive online presence.
+                </Typography>
+              </Box>
+            </Box>
+          </Stack>
+        </Stack>
+        <Stack
+          sx={{
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <Stack
+            mt={5}
+            sx={{
+              backgroundColor: "#383735",
+              alignContent: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              mx: "auto",
+              textAlign: "center",
+              width: "70%",
+              padding: "40px",
+            }}
+          >
+            {" "}
+            <Box
+              sx={{
+                alignContent: "start",
+                justifyContent: "flex-start",
+                flexDirection: "column",
+                textAlign: "start",
+              }}
+            >
+              <Typography variant="h6" color="#DAC5A7" fontSize="13px">
+                SEO & content{" "}
+              </Typography>
+              <Typography variant="h4" color="#DAC5A7" fontSize="25px">
+                Boosting Your Website's Organic Search Traffic
+              </Typography>
+            </Box>
+            <Box mt={2} sx={{ width: "100%", height: "100%" }}>
+              <img width="100%" height="100%" src="assets/servicesimg4.jfif" />
+            </Box>
+            <Box mt={5} width="100%" height="100%">
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Typography
+                  color="#DAC5A7"
+                  textAlign="start"
+                  width="250px"
+                  height="21px"
+                >
+                  Research
+                </Typography>
+                <Typography
+                  color="#DAC5A799"
+                  textAlign="start"
+                  width="814px"
+                  height="58px"
+                >
+                  I conduct thorough research to identify the best keywords and
+                  strategies to improve your website's search engine ranking.
+                </Typography>
+              </Box>
+              <Divider
+                sx={{
+                  borderColor: "#DAC5A726",
+                  margin: "20px",
+                  height: "100%",
+                }}
+              />
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Typography
+                  color="#DAC5A7"
+                  textAlign="start"
+                  width="250px"
+                  height="21px"
+                >
+                  SEo Ranking
+                </Typography>
+                <Typography
+                  color="#DAC5A799"
+                  textAlign="start"
+                  width="814px"
+                  height="58px"
+                >
+                  My SEO services are designed to improve your website's
+                  visibility on search engines, increasing your organic traffic
+                  and driving more leads and sales.
+                </Typography>
+              </Box>
+              <Divider
+                sx={{
+                  borderColor: "#DAC5A726",
+                  margin: "20px",
+                  height: "100%",
+                }}
+              />
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Typography
+                  color="#DAC5A7"
+                  textAlign="start"
+                  width="250px"
+                  height="21px"
+                >
+                  SEO Support
+                </Typography>
+                <Typography
+                  color="#DAC5A799"
+                  textAlign="start"
+                  width="814px"
+                  height="58px"
+                >
+                  With ongoing SEO support, I ensure that your website stays
+                  up-to-date with the latest SEO best practices, keeping you
+                  ahead of the competition.
+                </Typography>
+              </Box>
+            </Box>
+          </Stack>
+        </Stack>
       </Stack>
     </>
   );
