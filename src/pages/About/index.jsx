cd62ad7e0848675d22d0009ba6cdeec43dd9623c
AmirrezMirshafiee <amirrezamirshafiee0@gmail.com>
2024-04-05 02:05:45 +0330

    Update index.jsx

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -156,6 +156,7 @@ export default function About() {
                     }}
                   />
                 </Button>
+                
               </Box>
             </Stack>
           </Box>
